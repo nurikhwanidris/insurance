3279db4398a200935bcf685c9d11558f0e593ff7
nurikhwanidris <35907238+nurikhwanidris@users.noreply.github.com>
2020-11-03 17:52:51 +0800

    insurance developed the receipt

---
+++ clients/receipt.php
@@ -0,0 +1,101 @@
+<?php
+
+//DB Conn
+include('../core/dbconn.php');
+
+// Get data from POST.
+$name = mysqli_real_escape_string($conn, $_POST['name']);
+$amountTxt = mysqli_real_escape_string($conn, $_POST['amountTxt']);
+$amountDigits = mysqli_real_escape_string($conn, $_POST['amountDigits']);
+$paymentFor = mysqli_real_escape_string($conn, $_POST['paymentFor']);
+$paymentType = mysqli_real_escape_string($conn, $_POST['paymentType']);
+$dateIssued = mysqli_real_escape_string($conn, $_POST['dateIssued']);
+$issuedBy = mysqli_real_escape_string($conn, $_POST['issuedBy']);
+$designation = mysqli_real_escape_string($conn, $_POST['designation']);
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Receipt</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+    <!-- Google Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">
+    <!-- JQuery -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js">
+    </script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/js/mdb.min.js">
+    </script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-md-12 mx-auto">
+                <div class="card border">
+                    <div class="card-body">
+                        <div class="col-md-2 mx-auto">
+                            <img src="img/logo-black.png" alt="" style="width: auto; height:80px;">
+                        </div>
+                        <h5 class="my-3 p-3">Official Receipt</h5>
+                        <div class="row px-3">
+                            <div class="col-md-8">
+                                <p class="border-bottom">Mr/Mrs/Ms : <b><?= $name; ?></b></p>
+                                <div class="row">
+                                    <div class="col-md-8">
+                                        <p class="border-bottom ">Received the Sum of : <b><?= $amountTxt; ?></b></p>
+                                    </div>
+                                    <div class="col-md-4 text-center">
+                                        <p class="border"><b>RM<?= $amountDigits; ?></b></p>
+                                    </div>
+                                </div>
+                                <p class="border-bottom">Payment by : <b><?= $paymentType; ?></b></p>
+                                <p class="border-bottom">Being Payment of : <b><?= $paymentFor; ?></b></p>
+                            </div>
+                            <div class="col-md-4 mx-auto">
+                                <div class="row">
+                                    <p class="m-0">Receipt No : <b> ET2020000101</b></p>
+                                    <p class="m-0"><small>CO. REG. 1074485-W KPL/LN: 7644 | MA4686</small></p>
+                                    <p class="m-0"><small>GST001952911360 | MOF : K22173423761661323</small></p>
+                                    <p class="m-0"><small>No. 243 B, Tingkat 2,</small></p>
+                                    <p class="m-0"><small>Jalan Bandar 13,</small></p>
+                                    <p class="m-0"><small>Taman Melawati,</small></p>
+                                    <p class="m-0"><small>53100 Kuala Lumpur</small></p>
+                                </div>
+                                <div class="row">
+                                    <p>
+                                        <small class="border-bottom">Date : <b><?= $dateIssued; ?></b></small><br>
+                                        <small class="border-bottom">Issued By : <b><?= $issuedBy; ?></b></small><br>
+                                        <small class="border-bottom">Designation : <b><?= $designation; ?></small></b>
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row px-3">
+                            <div class="col-md-12">
+                                <p><small>N.B : Validity of this receipt subject to clearing cheque.</small><br><small>This is computer generated receipt. It does not require any signature.</small></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
--- clients/view.php
+++ clients/view.php
@@ -49,95 +49,97 @@ $row = mysqli_fetch_array($result);
         </div>
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <div class="card my-4">
-                    <div class="card-header text-center">
-                        Create Receipt
+                <form action="receipt.php" class="md-form form-sm" method="POST">
+                    <div class="card my-4">
+                        <div class="card-header text-center">
+                            Create Receipt
+                        </div>
+                        <div class="card-body">
+                            <form action="<?php $_SERVER['PHP_SELF']; ?>" class="md-form form-sm " method="POST">
+                                <div class="col-md-12">
+                                    <p class="lead">Client's Personal Details</p>
+                                    <small class="text-muted">Everything else is already pre-filled.</small>
+                                </div>
+                                <div class="col row">
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="name" id="name" class="form-control" value="<?= $row['name']; ?>" required>
+                                            <label for="name">Client's Name</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="ic" id="ic" class="form-control" value="<?= $row['ic']; ?>" required>
+                                            <label for="ic">NRIC Number without '-'</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col row">
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="amountTxt" id="ic" class="form-control" required>
+                                            <label for="ic">Amount in text</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" class="text form-control" name="amountDigits">
+                                            <label for="">Amount in digits</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col row">
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="paymentFor" id="paymentFor" class="form-control">
+                                            <label for="paymentFor">Being Payment</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <select name="paymentType" id="paymentType" class="md-form form-control">
+                                                <option value="">Select Payment Method</option>
+                                                <option value="Cash">Cash</option>
+                                                <option value="Credit Card">Credit Card</option>
+                                                <option value="Debit Card">Debit Card</option>
+                                                <option value="Cheque">Cheque</option>
+                                                <option value="Online Banking">Online Banking</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col row">
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="dateIssued" id="" class="form-control">
+                                            <label for="">dd/mm/yyyy</label>
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="issuedBy" id="" class="form-control">
+                                            <label for="">Issued By</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col row">
+                                    <div class="col-md-6 mb-0">
+                                        <div class="md-form">
+                                            <input type="text" name="designation" id="" class="form-control">
+                                            <label for="">Designation</label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col row mx-auto text-center">
+                                    <div class="col-md-12 mx-auto ">
+                                        <button name="submit" type="submit" class="btn btn-success btn-md">Submit</button>
+                                        <button name="reset" type="reset" class="btn btn-danger btn-md">Reset</button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
                     </div>
-                    <div class="card-body">
-                        <form action="<?php $_SERVER['PHP_SELF']; ?>" class="md-form form-sm " method="POST">
-                            <div class="col-md-12">
-                                <p class="lead">Client's Personal Details</p>
-                                <small class="text-muted">Everything else is already pre-filled.</small>
-                            </div>
-                            <div class="col row">
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="name" id="name" class="form-control" value="<?= $row['name']; ?>" required>
-                                        <label for="name">Client's Name</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="ic" id="ic" class="form-control" value="<?= $row['ic']; ?>" required>
-                                        <label for="ic">NRIC Number without '-'</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col row">
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="ic" id="ic" class="form-control" required>
-                                        <label for="ic">Amount in text</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" class="text form-control">
-                                        <label for="">Amount in digits</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col row">
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="paymentFor" id="paymentFor" class="form-control">
-                                        <label for="paymentFor">Being Payment</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <select name="state" id="state" class="md-form form-control">
-                                            <option value="">Select Payment Method</option>
-                                            <option value="Cash">Cash</option>
-                                            <option value="Credit Card">Credit Card</option>
-                                            <option value="Debit Card">Debit Card</option>
-                                            <option value="Cheque">Cheque</option>
-                                            <option value="Online Banking">Online Banking</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col row">
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="dateIssued" id="" class="form-control">
-                                        <label for="">dd/mm/yyyy</label>
-                                    </div>
-                                </div>
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="issuedBy" id="" class="form-control">
-                                        <label for="">Issued By</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col row">
-                                <div class="col-md-6 mb-0">
-                                    <div class="md-form">
-                                        <input type="text" name="designation" id="" class="form-control">
-                                        <label for="">Designation</label>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col row mx-auto text-center">
-                                <div class="col-md-12 mx-auto ">
-                                    <button name="submit" type="submit" class="btn btn-success btn-md">Submit</button>
-                                    <button name="reset" type="reset" class="btn btn-danger btn-md">Reset</button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
+                </form>
             </div>
         </div>
     </div>
